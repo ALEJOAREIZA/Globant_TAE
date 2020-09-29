6112460ad4dfd80ff0edee87d3521b639dd6d371
Alejandro Areiza guzman <Ing.alejandroareiza@Gmail.com>
2020-09-29 02:59:38 -0500

    add priority to tests

--- ESPN/src/test/java/com/espn/automation/tests/EspnTests.java
+++ ESPN/src/test/java/com/espn/automation/tests/EspnTests.java
@@ -9,49 +9,49 @@ import com.espn.automation.pages.EspnHomePage;
 import com.espn.automation.commonMethods.commonMethods;
 
 public class EspnTests extends BaseTests {
-	WebDriverWait mywait;
 
-//	@Test
-//	public void testLogInPopUp() {
-//		EspnHomePage _espnHomePage = espnHomePage();
-//		commonMethods _commonMethods = commonMethods();
-//		boolean isClickable = false;
-//		_commonMethods.click(_espnHomePage.userIcon);
-//		_commonMethods.click(_espnHomePage.loginOption);
-//		_commonMethods.changetoFrame(_espnHomePage.logInFrame);
-//		Assert.assertEquals(_commonMethods.getMessages(_espnHomePage.logInPopUp),
-//				"Log In\n" +
-//						"Need help logging in?\n" +
-//						"Facebook Log In is no longer available. For help logging in, please use the link above.\n" +
-//						"Sign Up");
-//		_commonMethods.click(_espnHomePage.closeXIcon);
-//		_commonMethods.exitfromFrame();
-//	}
-//	@Test
-//	public void testSigUpPopUp() {
-//		EspnHomePage _espnHomePage = espnHomePage();
-//		commonMethods _commonMethods = commonMethods();
-//		_commonMethods.click(_espnHomePage.userIcon);
-//		_commonMethods.click(_espnHomePage.loginOption);
-//		_commonMethods.changetoFrame(_espnHomePage.logInFrame);
-//		_commonMethods.click(_espnHomePage.signUpButton);
-//		Assert.assertEquals(_commonMethods.getMessages(_espnHomePage.SignInPopUp),"Create Account\n" +
-//				"Show password\n" +
-//				"Please send me occasional information and offers about my favorite teams, sports and other information from:\n" +
-//				"ESPN, ESPN+ and The Walt Disney Family of Companies\n" +
-//				"By creating an account, I agree to the Terms of Use and acknowledge that I have read the Privacy Policy.\n" +
-//				"My home country/region: Colombia. Change.\n" +
-//				"Sign Up\n" +
-//				"Already have an account? Log In");
-//		_commonMethods.exitfromFrame();
-//	}
-	@Test
+	@Test(priority=1)
+	public void testLogInPopUp() {
+		EspnHomePage _espnHomePage = espnHomePage();
+		commonMethods _commonMethods = commonMethods();
+		boolean isClickable = false;
+		_commonMethods.click(_espnHomePage.userIcon);
+		_commonMethods.click(_espnHomePage.loginOption);
+		_commonMethods.changetoFrame(_espnHomePage.logInFrame);
+		Assert.assertEquals(_commonMethods.getMessages(_espnHomePage.logInPopUp),
+				"Log In\n" +
+						"Need help logging in?\n" +
+						"Facebook Log In is no longer available. For help logging in, please use the link above.\n" +
+						"Sign Up");
+		_commonMethods.click(_espnHomePage.closeXIcon);
+		_commonMethods.exitfromFrame();
+	}
+	@Test(priority=2)
+	public void testSigUpPopUp() {
+		EspnHomePage _espnHomePage = espnHomePage();
+		commonMethods _commonMethods = commonMethods();
+		_commonMethods.click(_espnHomePage.userIcon);
+		_commonMethods.click(_espnHomePage.loginOption);
+		_commonMethods.changetoFrame(_espnHomePage.logInFrame);
+		_commonMethods.click(_espnHomePage.signUpButton);
+		Assert.assertEquals(_commonMethods.getMessages(_espnHomePage.SignInPopUp),"Create Account\n" +
+				"Show password\n" +
+				"Please send me occasional information and offers about my favorite teams, sports and other information from:\n" +
+				"ESPN, ESPN+ and The Walt Disney Family of Companies\n" +
+				"By creating an account, I agree to the Terms of Use and acknowledge that I have read the Privacy Policy.\n" +
+				"My home country/region: Colombia. Change.\n" +
+				"Sign Up\n" +
+				"Already have an account? Log In");
+		_commonMethods.click(_espnHomePage.closeXIcon);
+		_commonMethods.exitfromFrame();
+	}
+	@Test(priority=3)
 	public void createAccount(){
 		EspnHomePage _espnHomePage = espnHomePage();
 		commonMethods _commonMethods = commonMethods();
 		String firstName="Alejandro";
 		String lastName="Areiza";
-		String email="ejemplo8@tae.com";
+		String email="ejemplo9@tae.com";
 		String password="HelloWorld1!";
 
 		_commonMethods.click(_espnHomePage.userIcon);
@@ -64,8 +64,7 @@ public class EspnTests extends BaseTests {
 		_commonMethods.enterText(password,_espnHomePage.password);
 		_commonMethods.click(_espnHomePage.SignUpbutton);
 		_commonMethods.exitfromFrame();
-		Assert.assertEquals(_commonMethods.isDisplayed(_espnHomePage.userIcon),true);
-//
+		Assert.assertTrue(_commonMethods.isClickable(_espnHomePage.userIcon));
 	}
 //	@Test
 //	public void test3(){
